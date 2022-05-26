ec7dba8ae963d484d3385086447bd14bd9921fa9
toby3adams <toby3adams@byui.edu>
2022-05-26 15:38:20 -0600

    added comments to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace jumper{
 
+    //main program
     class program{
 
+        //entry point for the program
         static void Main(string[] args){
 
             Director director = new Director();
